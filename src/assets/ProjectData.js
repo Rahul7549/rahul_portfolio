c808b43084fe4014f04108ca856321cfa7814eaf
Rahul Kumar Sharma <rahul.sharma@example.com>
2026-02-16 13:26:04 +0530

    portfolio update at feb 2026

--- src/assets/ProjectData.js
+++ src/assets/ProjectData.js
@@ -1,6 +1,6 @@
 
-import curatusImg from './curatus.jpg'
-import screenCareImg from './screenCare.jpeg'
+// import curatusImg from './curatus.jpg'
+// import screenCareImg from './screenCare.jpeg'
 import covidTrakerImg from './covidTrakerApp.png'
 import netflixImg from './netflixClone.png'
 import weatherStatusImg from './weatherApplication.png'
@@ -8,65 +8,6 @@ import RequestEase from './RequestEase.png'
 import adminPanelImg from './admin-panel.png'
 
 
-// const projectCardData=[
-//     {
-//         imgsrc:netflixImg,
-//         title:"NETFLIX CLONE:",
-//         about:"Created a Netflix clone using ReactJS and consuming prebuilt TMDB APIs using axios and hosted it on Firebase Country",
-//         view:"https://netflix-clone-e7320.web.app/",
-//         source:'https://github.com/Rahul7549/netflix_clone'
-//     },
-//     {
-//         imgsrc:adminPanelImg,
-//         title:'Admin panel to track shipment:',
-//         about:'This Admin Panel, built with Angular 16, allows seamless management of users, products, and shipments. It features an intuitive interface for editing product details, tracking shipments with imgUrl and updatedOn fields, and managing users with role-based access. The modular design, with admin and auth modules, ensures code efficiency, while popup dialogs maintain workflow visibility. Responsive and scalable, it provides a powerful backend management solution.',
-//         view:'https://github.com/Rahul7549/shipment-admin-ui',
-//         source:"https://github.com/Rahul7549/shipment-admin-ui"
-//     },
-    
-//     {
-//         imgsrc:RequestEase,
-//         title:'RequestEase:',
-//         about:'The application features a user-friendly architecture, comprising specific modules for administrators and regular users. Normal users can easily request service activation through an intuitive interface, simplifying navigation and selection of desired services. Additionally, administrators have access to all requests across the application, enabling them to approve or deny requests.',
-//         view:'https://rahul7549.github.io/user-service.github.io/',
-//         source:"https://github.com/Rahul7549/user-service.github.io"
-//     },
-    
-//     // {
-//     //     imgsrc:screenCareImg,
-//     //     title:'SKIN CARE PRODUCT PERFORMANCE ANALYST:',
-//     //     about:'This web Application is developed using Angular and Spring MVC technology that help the Analys to analyse the effect of Unileaver’s skin care product performance’s on different geometric area.',
-//     //     view:'',
-//     //     source:''
-//     // },
-    
-//     {
-//         imgsrc:covidTrakerImg,
-//         title:'COVID TRACKER WEB APP:',
-//         about:'Made ReactJs personal project that help to track the current covid status across the world and Representing using bubble graph, there are 4 module and those are Coronavirus Cases, Recovered,Deaths and Live Cases by Country.',
-//         view:'https://rahul7549.github.io/covid-tracker/',
-//         source:"https://github.com/Rahul7549/covid-tracker"
-//     },
-//     // {
-//     //     imgsrc:curatusImg,
-//     //     title:'CURATUS:',
-//     //     about:'Curatus acts as a central repository for curated documents, especially patent-focused technology or competitor analyses. It streamlines the sharing process via email by housing tagged, annotated, and cataloged documents. It fosters easy collaboration among stakeholders',
-//     //     // about:'Curatus is a repository of curated documents that are a result of technology or competitor analysis. A significant portion of these analyses are focused on patents and the findings from these analyses are typically shared over email and may vary from request to request. Curatus attempts to streamline this process by serving as a central repository where all these documents (suitably tagged, annotated and catalogued) and allowing for simple collaboration across all stakeholders.',
-//     //     view:'https://curatus.biginfolabs.com/',
-//     //     source:"https://biginfolabs.com/"
-        
-//     // },
-//     {
-//         imgsrc:weatherStatusImg,
-//         title:'Weather Application:',
-//         about:'Made ReactJs personal project that help to track the weather status in period of three hour cross the cities.',
-//         view:'https://rahul7549.github.io/weather-status/',
-//         source:"https://github.com/Rahul7549/weather-status"
-//     },
-    
-    
-    
-// ]
 
 const projectCardData = [
   {
